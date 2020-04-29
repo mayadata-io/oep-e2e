7f48c32fa623a9d109147fbe638c8d86002d2fed
AmitKumarDas <amit.das@mayadata.io>
2020-04-29 14:31:47 +0530

    feat(director): add install upgrade testplans of director
    Signed-off-by: AmitKumarDas <amit.das@mayadata.io>

--- testplan/sidebars.js
+++ testplan/sidebars.js
@@ -7,13 +7,6 @@ module.exports = {
 		        label: 'Install and Upgrade',
 		        items: [
 				'director/installation/installation',
-				'director/installation/tc-install-gpd-std',
-				'director/installation/tc-install-ssd-lpv',
-				'director/installation/tc-install-gpd-lpv',
-				'director/installation/tc-install-ssd-cstor',
-				'director/installation/tc-upgrade-gpd-std',
-				'director/installation/tc-upgrade-ssd-lpv',
-				'director/installation/tc-upgrade-ssd-cstor'
 			],
 		},
 		{
