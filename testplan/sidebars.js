4e6c08cb296db12157fd2da4fc65f0d818b1c6b7
Amit Bhatt <amitbhatt818@gmail.com>
2020-05-05 19:12:02 +0530

    Added files in sidebar.js
    Signed-off-by: Amit Bhatt <amitbhatt818@gmail.com>

--- testplan/sidebars.js
+++ testplan/sidebars.js
@@ -1,108 +1,108 @@
 module.exports = {
-  someSidebar: {
-    Overview: ['getstarted', 'platforms', 'testbed', 'pipelines','compatibility'],
-    Director: [
-		{
-		        type: 'category',
-		        label: 'Install and Upgrade',
-		        items: [
-				'director/installation/installation',
-			],
-		},
-		{
-		        type: 'category',
-		        label: 'Workflow',
-		        items: [
-				'director/workflow/onboarding',
-				'director/workflow/test1'
-			],
-		},
-		{
-		        type: 'category',
-		        label: 'Authentication',
-		        items: [
-				'director/authentication/authentication',
-			],
-		},
-		{
-		        type: 'category',
-		        label: 'Teaming',
-		        items: [
-				'director/teaming/directorTeaming',
-				'director/teaming/TC-Team-InviteUser',
-				'director/teaming/TC-Team-RoleChange',
-				'director/teaming/TC-Team-RoleChange-Negative'
-			],
-		},
-		{
-		        type: 'category',
-		        label: 'OpenEBS Provisioning',
-		        items: [
-                		'director/openebs-provisioning/provisioning',
-		                'director/openebs-provisioning/TCID-DIR-OP-DEVICE-VIEW-ALL',
-                		'director/openebs-provisioning/TCID-DIR-OP-DEVICE-VIEW-SCALE',
-		                'director/openebs-provisioning/TCID-DIR-OP-DEVICE-VIEW-SCALE-RESTART',
-                		'director/openebs-provisioning/TCID-DIR-OP-DEVICE-FILTER-SSD',
-		                'director/openebs-provisioning/TCID-DIR-OP-DEVICE-FILTER-PATH',
-                		'director/openebs-provisioning/TCID-DIR-OP-DEVICE-IGNORE',
-		                'director/openebs-provisioning/TCID-DIR-OP-DEVICE-VIEW-TOGGLE',
-                		'director/openebs-provisioning/TCID-DIR-OP-CSTOR-POOL-RECOMMEND-LIST',
-		                'director/openebs-provisioning/TCID-DIR-OP-CSTOR-POOL-RECOMMEND-LIST-NO-NDM',
-						'director/openebs-provisioning/TCID-DIR-OP-CSTOR-POOL-RECOMMEND-LIST-STRIPE',
-						'director/openebs-provisioning/TCID-DIR-OP-CSTOR-POOL-RECOMMEND-LIST-RAIDZ',
-						'director/openebs-provisioning/TCID-DIR-OP-CSTOR-POOL-RECOMMEND-CREATE-RAIDZ',
-		                'director/openebs-provisioning/TCID-DIR-OP-CSTOR-POOL-RECOMMEND-CREATE-STRIPE',
-                		'director/openebs-provisioning/TCID-DIR-OP-CSTOR-POOL-RECOMMEND-LIST-MIRROR',
-		                'director/openebs-provisioning/TCID-DIR-OP-CSTOR-POOL-RECOMMEND-CREATE-MIRROR'
-			],
-		},
-		{
-		        type: 'category',
-		        label: 'DMaaS',
-		        items: [
-				'director/dmaas/dmaas',
-			],
-		},
-		{
-		        type: 'category',
-		        label: 'Logging',
-		        items: [
-				'director/logging/logging',
-				'director/logging/test1'
-			],
-		},
-		{
-		        type: 'category',
-		        label: 'Visualization',
-		        items: [
-				'director/visualization/visualization',
-				'director/visualization/test1'
-			],
-		},
-		{
-		        type: 'category',
-		        label: 'Alerts',
-		        items: [
-				'director/alerts/alerts',
-			],
-		},
-    ],
-    "OpenEBS Enterprise": [
-		{
-		        type: 'category',
-		        label: 'OpenEBS Enterprise',
-		        items: [
-				    'openebs-enterprise/openebs/openebs'
-			    ],
-		},
-		{
-		        type: 'category',
-		        label: 'DAOs',
-		        items: [
-				    'openebs-enterprise/dao/dao'
-			    ],
-        	},
-    ],	  
-    Help: ['doc1', 'mdx'],
-  },
+	someSidebar: {
+		Overview: ['getstarted', 'platforms', 'testbed', 'pipelines', 'compatibility'],
+		Director: [
+			{
+				type: 'category',
+				label: 'Install and Upgrade',
+				items: [
+					'director/installation/installation',
+				],
+			},
+			{
+				type: 'category',
+				label: 'Workflow',
+				items: [
+					'director/workflow/onboarding',
+					'director/workflow/test1'
+				],
+			},
+			{
+				type: 'category',
+				label: 'Authentication',
+				items: [
+					'director/authentication/authentication',
+				],
+			},
+			{
+				type: 'category',
+				label: 'Teaming',
+				items: [
+					'director/teaming/directorTeaming',
+					'director/teaming/TC-Team-InviteUser',
+					'director/teaming/TC-Team-RoleChange',
+					'director/teaming/TC-Team-RoleChange-Negative'
+				],
+			},
+			{
+				type: 'category',
+				label: 'OpenEBS Provisioning',
+				items: [
+					'director/openebs-provisioning/provisioning',
+					'director/openebs-provisioning/TCID-DIR-OP-DEVICE-VIEW-ALL',
+					'director/openebs-provisioning/TCID-DIR-OP-DEVICE-VIEW-SCALE',
+					'director/openebs-provisioning/TCID-DIR-OP-DEVICE-VIEW-SCALE-RESTART',
+					'director/openebs-provisioning/TCID-DIR-OP-DEVICE-FILTER-SSD',
+					'director/openebs-provisioning/TCID-DIR-OP-DEVICE-FILTER-PATH',
+					'director/openebs-provisioning/TCID-DIR-OP-DEVICE-IGNORE',
+					'director/openebs-provisioning/TCID-DIR-OP-DEVICE-VIEW-TOGGLE',
+					'director/openebs-provisioning/TCID-DIR-OP-CSTOR-POOL-RECOMMEND-LIST',
+					'director/openebs-provisioning/TCID-DIR-OP-CSTOR-POOL-RECOMMEND-LIST-NO-NDM',
+					'director/openebs-provisioning/TCID-DIR-OP-CSTOR-POOL-RECOMMEND-LIST-STRIPE',
+					'director/openebs-provisioning/TCID-DIR-OP-CSTOR-POOL-RECOMMEND-LIST-RAIDZ',
+					'director/openebs-provisioning/TCID-DIR-OP-CSTOR-POOL-RECOMMEND-CREATE-RAIDZ',
+					'director/openebs-provisioning/TCID-DIR-OP-CSTOR-POOL-RECOMMEND-CREATE-STRIPE',
+					'director/openebs-provisioning/TCID-DIR-OP-CSTOR-POOL-RECOMMEND-LIST-MIRROR',
+					'director/openebs-provisioning/TCID-DIR-OP-CSTOR-POOL-RECOMMEND-CREATE-MIRROR'
+				],
+			},
+			{
+				type: 'category',
+				label: 'DMaaS',
+				items: [
+					'director/dmaas/dmaas',
+				],
+			},
+			{
+				type: 'category',
+				label: 'Logging',
+				items: [
+					'director/logging/logging',
+					'director/logging/test1'
+				],
+			},
+			{
+				type: 'category',
+				label: 'Visualization',
+				items: [
+					'director/visualization/visualization',
+					'director/visualization/test1'
+				],
+			},
+			{
+				type: 'category',
+				label: 'Alerts',
+				items: [
+					'director/alerts/alerts',
+				],
+			},
+		],
+		"OpenEBS Enterprise": [
+			{
+				type: 'category',
+				label: 'OpenEBS Enterprise',
+				items: [
+					'openebs-enterprise/openebs/openebs'
+				],
+			},
+			{
+				type: 'category',
+				label: 'DAOs',
+				items: [
+					'openebs-enterprise/dao/dao'
+				],
+			},
+		],
+		Help: ['doc1', 'mdx'],
+	},
 };
