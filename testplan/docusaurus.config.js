3257bd172c96c79d60494483d45f2d59aa600147
bhaskarhc <hcbhaskar7@gmail.com>
2020-07-27 15:08:07 +0530

    version fix

--- testplan/docusaurus.config.js
+++ testplan/docusaurus.config.js
@@ -13,7 +13,7 @@ module.exports = {
         alt: 'MayaData Kubera E2E',
         src: 'img/logo.png',
       },
-      items: [
+      links: [
         {
           to: 'docs/getstarted',
           activeBasePath: 'docs',
